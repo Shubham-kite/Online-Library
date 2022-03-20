8fe0cc2915ba07794525db34a242fb19fd1595d1
kunalavghade <kunalavghade77@example.com>
2022-03-20 23:02:22 +0530

    v2

--- Onlinelib/library/admin.py
+++ Onlinelib/library/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from library.models import Student,books
-
+from . models import Student,books,ContactUs
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ('name','email','phone')
+	list_display = ('firstName','firstName','email','phone','password')
 
 
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('book_name','authour')
 
+class ContactUsAdmin(admin.ModelAdmin):
+	list_display = ("your_name","email_address","Your_message")
+
 admin.site.register(Student,StudentAdmin)
 admin.site.register(books,BookAdmin)
+admin.site.register(ContactUs,ContactUsAdmin)
